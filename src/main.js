68f9572fa6e8ab4b6c68f4b11b56a0909d9d9d03
saken <saken.bolatzhanuly@gmail.com>
2019-10-28 00:14:44 +0600

    Created two layout: empty and main

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
