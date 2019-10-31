af8bef6f4a7dddfa525dcbfaecb9a8ba9a4e4c46
saken <saken.bolatzhanuly@gmail.com>
2019-11-01 00:12:42 +0600

    Validation with Vuelidate

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
+import Vuelidate from "vuelidate/src";
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dateFilter from "./filters/date.filter";
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
-import dateFilter from "./filters/date.filter";
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuelidate);
 Vue.filter('dateFilter', dateFilter);
 
 new Vue({
