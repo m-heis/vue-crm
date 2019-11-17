c7d2441e3664b73d41ae156ecb47a10d69e0371b
saken <saken.bolatzhanuly@gmail.com>
2019-11-18 01:58:46 +0600

    Planning page and Tooltip

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,7 +15,7 @@
                 </div>
             </main>
 
-            <div class="fixed-action-btn">
+            <div class="fixed-action-btn" title="Создать новую запись">
                 <router-link class="btn-floating btn-large blue" to="/record">
                     <i class="large material-icons">add</i>
                 </router-link>
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -3,19 +3,24 @@
     <div>
         <div class="page-title">
             <h3>Планирование</h3>
-            <h4>12 212</h4>
+            <h4>{{info.bill}}</h4>
         </div>
 
-        <section>
-            <div>
+        <Loader v-if="loading" />
+
+        <p class="center" v-else-if="!categories.length">Категорий пока нет. <router-link to="categories">Добавить новую категорию</router-link></p>
+
+        <section v-else>
+            <div v-for="c in categories" :key="c.id">
                 <p>
-                    <strong>Девушка:</strong>
-                    12 122 из 14 0000
+                    <strong>{{c.title}}:</strong>
+                    {{c.spend}} из {{c.limit}}
                 </p>
-                <div class="progress" >
+                <div class="progress" v-tooltip="c.tooltip">
                     <div
-                            class="determinate green"
-                            style="width:40%"
+                            class="determinate"
+                            :class="[c.progressColor]"
+                            :style="{width: c.progressPercent + '%'}"
                     ></div>
                 </div>
             </div>
@@ -25,8 +30,49 @@
 </template>
 
 <script>
+
+    import {mapGetters} from 'vuex'
+
     export default {
-        name: "Planning"
+        name: "Planning",
+        data: () => ({
+            loading: true,
+            categories: []
+        }),
+        computed: {
+            ...mapGetters(['info'])
+        },
+        async mounted() {
+            const records = await this.$store.dispatch('fetchRecords');
+            const categories2 = await this.$store.dispatch('fetchCategories');
+
+            this.categories = categories2.map(cat => {
+                const spend = records
+                    .filter(r => r.categoryId === cat.id)
+                    .filter(r => r.type === 'outcome')
+                    .reduce((total, record) => {
+                        return total += +record.amount
+                    }, 0);
+
+                const percent = 100 * spend / cat.limit;
+                const progressPercent = percent > 100 ? 100 : percent;
+                const progressColor =
+                    percent < 60 ? 'green' : percent < 100 ? 'yellow' : 'red';
+
+                const tooltipValue = cat.limit - spend;
+                const tooltip = `${tooltipValue < 0 ? 'Превышение на' : 'Осталось'} ${Math.abs(tooltipValue)}`;
+
+                return {
+                    ...cat,
+                    progressPercent,
+                    progressColor,
+                    spend,
+                    tooltip
+                }
+            });
+
+            this.loading = false
+        }
     }
 </script>
 
